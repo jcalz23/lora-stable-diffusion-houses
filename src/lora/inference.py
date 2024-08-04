b15503bfd270c3b356e83d8de65e72128f2e9987
jcalz23 <j.calzaretta.ai@gmail.com>
2024-08-04 13:22:51 -0700

    Successful fine-tuning on houses

--- src/lora/inference.py
+++ src/lora/inference.py
@@ -1,11 +1,11 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-model_path = "sd-naruto-model-lora"
+model_path = "housing-lora"
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
 pipe.unet.load_attn_procs(model_path)
 pipe.to("cuda")
 
-prompt = "A naruto with green eyes and red legs."
+prompt = "A Greek contemporary home with flat roof and water view"
 image = pipe(prompt, num_inference_steps=30, guidance_scale=7.5).images[0]
-image.save("naruto.png")
+image.save("image.png")
--- src/lora/run_lora.sh
+++ src/lora/run_lora.sh
@@ -1,13 +1,14 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export DATASET_NAME="lambdalabs/naruto-blip-captions"
+#export DATASET_NAME="lambdalabs/naruto-blip-captions"
 
 accelerate launch --mixed_precision="no" train_text_to_image_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
-  --dataset_name=$DATASET_NAME --caption_column="text" \
+  --caption_column="text" \
   --resolution=256 --random_flip \
   --train_batch_size=1 \
   --num_train_epochs=1 --checkpointing_steps=5000 \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
-  --output_dir="sd-naruto-model-lora" \
-  --validation_prompt="cute dragon creature" --report_to="wandb"
+  --output_dir="housing-lora" \
+  --validation_prompt="Greek modern house with pool" --report_to="wandb" \
+  --train_data_dir="/root/lora-stable-diffusion-houses/data/"
